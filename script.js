df3f2182033f83d7cb0087f2a76145d547ec6a23
deekshithdreddy <deekshithdreddy13@gmail.com>
2025-05-31 19:30:25 +0530

    added skills section

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ document.addEventListener('mousemove', (e) => {
     cursor.style.left = e.clientX + 'px';
     cursor.style.top = e.clientY + 'px';
     
+    
     setTimeout(() => {
         cursorFollower.style.left = e.clientX + 'px';
         cursorFollower.style.top = e.clientY + 'px';
